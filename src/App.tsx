3046aadb7d8a62859fc5cb2b3dd48d5e293d633e
rel <chuongior@hotmail.com>
2024-01-25 12:54:15 +0700

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
 import { AppProvider } from '@/provider/app';
 import { Provider } from 'react-redux';
 import { store } from '@/store/store';
 import { AppRoutes } from '@/routes';
 import { AxiosErrorComponent } from '@/components/Layout';
+import ToggleColorModeProvider from './context/color-mode-context';
 
 function App() {
   return (
-    <Provider store={store}>
-      <AxiosErrorComponent />
-      <AppProvider>
-        <AppRoutes />
-      </AppProvider>
-    </Provider>
+    <ToggleColorModeProvider>
+      <Provider store={store}>
+        <AxiosErrorComponent />
+        <AppProvider>
+          <AppRoutes />
+        </AppProvider>
+      </Provider>
+    </ToggleColorModeProvider>
   );
 }
 

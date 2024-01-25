2449277601c90f3c6897615fff46c0c9b70f2b28
rel <chuongior@hotmail.com>
2024-01-25 12:57:57 +0700

    add theme change feature context

---
+++ src/context/color-mode-context.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+export default function ToggleColorModeProvider(props: any) {
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}

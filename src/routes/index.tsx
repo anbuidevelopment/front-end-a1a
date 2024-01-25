a464c5fe23e96d4caef63fb772b8a3b591c9d9c7
rel <chuongior@hotmail.com>
2024-01-25 12:54:52 +0700

    update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,9 @@
 import { useRoutes } from 'react-router-dom';
 
-// import { Landing } from '@/features/misc';
-// import { useAuth } from '@/lib/auth';
 import { protectedRoutes } from './protected';
-import { publicRoutes } from './public';
-import { Login } from '@/features/auth/routes/Login';
-import { useUser } from '@/lib/auth';
 
 export const AppRoutes = () => {
-  const auth = useUser();
-
-  const commonRoutes = [{ path: '/', element: <Login /> }];
-
-  const routes = (auth.data != null) ? protectedRoutes : publicRoutes;
-
-  const element = useRoutes([...routes, ...commonRoutes]);
+  const element = useRoutes([...protectedRoutes]);
 
   return <>{element}</>;
 };
